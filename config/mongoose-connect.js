af2195eb18da70a57d180afc5b6dfcca2d5f9d5d
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-12-22 18:36:18 +0530

    everything work properly

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const dbgr = require("debug")("development:mongoose");
+const dbgr = require("debug")(process.env.DEBUG || "development:mongoose");
 
 if (process.env.NODE_ENV === "development") {
   mongoose.set("debug", true);
 }
+
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://127.0.0.0:27017/khaatabook")
-  .then(function () {
-    dbgr("connected to Mongo");
+  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/khaatabook")
+  .then(() => {
+    dbgr("Connected to MongoDB");
   })
-  .catch(function (err) {
-    dbgr(err);
+  .catch((err) => {
+    dbgr("Connection error:", err);
   });
 
 let db = mongoose.connection;
